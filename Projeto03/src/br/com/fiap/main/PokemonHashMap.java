1d9b1be3c1dd9523b57003d0064aa18c79feede6
ThiagoMoota <thiago_mota123@hotmail.com>
2025-08-11 09:36:19 -0300

    Aula do dia 11-08-2025 com Collections Framework.

---
+++ Projeto03/src/br/com/fiap/main/PokemonHashMap.java
@@ -0,0 +1,26 @@
+package br.com.fiap.main;
+
+import javax.swing.*;
+import java.util.HashMap;
+
+public class PokemonHashMap {
+    public static void main(String[] args) {
+        HashMap<String, String> mapa = new HashMap<>();
+        do {
+            try {
+                String nome, tipo;
+                do {
+
+                } while (){
+
+                }
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+        } while (JOptionPane.showConfirmDialog(null, "Deseja continuar?", "Atenção", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == 0);
+        JOptionPane.showMessageDialog(null, "Fim de programa!", "Adeus", JOptionPane.WARNING_MESSAGE);
+            }
+        }
+    }
+}
