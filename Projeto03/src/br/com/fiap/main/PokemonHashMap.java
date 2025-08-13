d907294bda6deab0675946a99994ba794e4df211
ThiagoMoota <thiago_mota123@hotmail.com>
2025-08-13 09:48:12 -0300

    Adicionando praticando do Projeto 3.

--- Projeto03/src/br/com/fiap/main/PokemonHashMap.java
+++ Projeto03/src/br/com/fiap/main/PokemonHashMap.java
@@ -5,22 +5,32 @@ import java.util.HashMap;
 
 public class PokemonHashMap {
     public static void main(String[] args) {
-        HashMap<String, String> mapa = new HashMap<>();
+        HashMap<String, String> pokemon = new HashMap<String, String>();
         do {
             try {
                 String nome, tipo;
                 do {
+                    nome = JOptionPane.showInputDialog("Digite o nome de um pokémon, ou caso queira encerrar digite \"fim\" ").toUpperCase();
+                        if (!nome.equals("FIM")){
+                            if (pokemon.containsKey(nome)){
+                                JOptionPane.showMessageDialog(null, "Este pokémon já foi cadastrado!");
+                            } else {
+                                tipo = JOptionPane.showInputDialog("Digite o tipo do pokémon informado anteriormente: ");
+                                pokemon.put(nome, tipo);
+                        }
+                    }
+                } while (!nome.equals("FIM"));
 
-                } while (){
-
+                String escolha = JOptionPane.showInputDialog("Digite o nome de um pokémon para saber o tipo ").toUpperCase();
+                if (pokemon.containsKey(escolha)){
+                    JOptionPane.showMessageDialog(null, "O tipo desse pokémon é: " + pokemon.get(escolha), "Nome do pokémon", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Pokémon não cadastrado!", "Atenção", JOptionPane.WARNING_MESSAGE);
                 }
-
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(null, e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
             }
         } while (JOptionPane.showConfirmDialog(null, "Deseja continuar?", "Atenção", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == 0);
         JOptionPane.showMessageDialog(null, "Fim de programa!", "Adeus", JOptionPane.WARNING_MESSAGE);
-            }
-        }
     }
 }
